b573e87bb7eee1d179eecc42f23190f8d431f49a
cbuxton1984 <cbuxton1984@gmail.com>
2022-05-06 09:15:29 +0100

    Added RHEL VM

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ module "subnet" {
   subnet_address_prefixes = var.subnet_address_prefixes
 }
 
-module "publicip" {
+module "publicipwin" {
   source = "github.com/cbuxton1984/terraform_modules/public_ip"
   pi_name = "windows-demo-vm-publicip"
   allocation_method = "Static"
@@ -36,19 +36,45 @@ module "publicip" {
   tags = var.tags
 }
 
-# Virtual Machines
+module "publiciprhel" {
+  source = "github.com/cbuxton1984/terraform_modules/public_ip"
+  pi_name = "windows-demo-vm-publicip"
+  allocation_method = "Static"
+  location = module.rg.rg_location
+  rg_name = module.rg.rg_name
+  tags = var.tags
+}
+
+# Virtual Machines Windows
 
 module "windows_vm_2019" {
   source ="github.com/cbuxton1984/terraform_modules/win_vm"
   vm_name = "windows-demo-vm"
   location = module.rg.rg_location
   rg_name = module.rg.rg_name
-  vm_size = "Standard_B2s"
+  vm_size = "Standard_B1s"
   vm_user = var.admuser
   vm_pass = var.admpass
   os_sku = "2019-Datacenter"
   os_version = "latest"
   subnet_id = module.subnet.subnet_id
-  publicip_id = module.publicip.publicip_id
+  publicip_id = module.publicipwin.publicip_id
+  tags = var.tags
+}
+
+# Virtual Machines RHEL
+
+module "rhel_vm" {
+  source ="github.com/cbuxton1984/terraform_modules/rhel_vm"
+  vm_name = "rhel-demo-vm"
+  location = module.rg.rg_location
+  rg_name = module.rg.rg_name
+  vm_size = "Standard_B1s"
+  vm_user = var.admuser
+  vm_pass = var.admpass
+  os_sku = "76-gen2"
+  os_version = "latest"
+  subnet_id = module.subnet.subnet_id
+  publicip_id = module.publiciprhel.publicip_id
   tags = var.tags
 }
